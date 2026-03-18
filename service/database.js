e3744124523ee51e9ef6b72065ad0707621cbf00
Andrew Harding <andrew.n.harding@icloud.com>
2026-03-17 22:29:03 -0600

    Added database login info and function to start and test connection

---
+++ service/database.js
@@ -0,0 +1,18 @@
+const { MongoClient } = reuqire("mongodb");
+const config = require("./dbConfig.json");
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db("users");
+const userCollection = db.collection("user");
+const logCollection = db.collection("datalog");
+
+(async function testConnection() {
+    try {
+        await db.command({ ping: 1});
+        console.log("Connect to database");
+    } catch (ex) {
+        console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+        process.exit(1);
+    }
+})();
